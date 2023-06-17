d76e59b745f80600fd03fcb63a3a3db8633a7cd8
Adielson Medeiros <medeirosadielson@gmail.com>
2023-06-17 13:45:20 -0300

    quase pronto

--- App.js
+++ App.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import { useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
 import {Picker} from '@react-native-picker/picker'
 
 export default function App() {
+
+
+/*const placeholder = {
+  label: 'Selecione uma moeda...',
+  value: null,
+  color: '#000'
+}*/
 
 const [moedaSelect, setMoedaSelecionada]= useState(0)
 const [moeda,setMoeda] = useState([
@@ -17,6 +24,8 @@ let MoedasItem = moeda.map( (v,k)  => {
 
 
 
+
+
   return (
     <View style={styles.container}>
       <View style={styles.areaMoeda}>
@@ -27,7 +36,20 @@ let MoedasItem = moeda.map( (v,k)  => {
       {MoedasItem}
 
       </Picker>
+      <View style={styles.areaValor}>
+      <Text style={styles.titulo}> Digite um valor para converter em R$: </Text>
+      <TextInput placeholder='Ex: 150' style={styles.input} keyboardType='numeric'/>
       </View>
+        <TouchableOpacity style={ styles.botaoArea}>
+          <Text style={styles.botaoTexto}> Converter </Text>
+        </TouchableOpacity>
+
+      </View>
+        <View style={styles.areaResultado}>
+          <Text style={styles.valorConvertido}> 5 usd</Text>
+          <Text style={[styles.valorConvertido, {fontSize: 18, margin: 10}]}> corresponde a </Text>
+          <Text style={styles.valorConvertido}> 1 milhao de reais </Text>
+        </View>
     </View>
   );
 }
@@ -35,14 +57,61 @@ let MoedasItem = moeda.map( (v,k)  => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#ddd',
+    backgroundColor: '#121212',
     alignItems: 'center',
     justifyContent: 'center',
-    paddingTop: 40,
+    paddingTop: 35,
     marginTop: 25,
   },
-  moeda:{
+  areaMoeda:{
     marginTop: 15,
-    fontSize: 35
+    backgroundColor: '#fff',
+    padding: 10,
+    width: 350,
+    borderRadius: 5,
+    marginBottom: 1,
+
   },
+  titulo:{
+    fontSize: 15,
+    color: '#000',
+    paddingTop: 5,
+    paddingLeft: 5,
+  },
+  input:{
+    backgroundColor: '#fff',
+    paddingTop: 5,
+    paddingLeft: 10,
+    fontSize: 15,
+    marginTop: 8,
+    color: '#000'
+
+  },
+  botaoArea:{
+    marginTop: 10,
+    backgroundColor: '#f84b57',
+    height: 45,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  botaoTexto:{
+    color: '#fff',
+    fontSize: 18,
+    fontWeight: 'bold',
+
+  },
+
+  areaResultado:{
+    marginTop: 35,
+    width: '90%',
+    backgroundColor: '#FFF',
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 25,
+  },
+  valorConvertido:{
+    fontSize: 39,
+    fontWeight: 'bold',
+    color: '#000'
+  }
 });
